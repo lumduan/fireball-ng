84b75214d222af8a7b047279309997a45570ca3a
batt <bad.sonsuk@gmail.com>
2023-12-24 21:50:58 +0700

    FIX: rm findstock-component > app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { FindStockComponent } from './find-stock/find-stock.component'
 import { IncomeStatementComponent } from './financial-statement/income-statement/income-statement.component';
 import { StockComponent } from './stock/stock.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, FindStockComponent, IncomeStatementComponent, StockComponent,],
+  imports: [CommonModule, RouterOutlet, IncomeStatementComponent, StockComponent,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
